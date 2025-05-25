d55c2095d00feecc8946722d5b4adc4c3f71f09a
arogo yugi <ayugi2001@gmail.com>
2025-05-25 21:44:35 +0300

    basic operations first commit

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 =5
+
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#[operation] of [number1] and [number2] is [result].
+
+print ("sum of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+print ("difference of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+print ("product of " + str(number1) + " and " + str(number2) + " is " + str(product))
